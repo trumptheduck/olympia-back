b3e6926b2b7528555b00638ee090894292de94bd
trumptheduck <trumptheduck@github.com>
2022-01-22 15:11:48 +0700

    first commit

---
+++ handlers/room.js
@@ -0,0 +1,38 @@
+
+// const rooms = [];
+
+// function makeid(length) {
+//   var result           = '';
+//   var characters       = '0123456789';
+//   var charactersLength = characters.length;
+//   for ( var i = 0; i < length; i++ ) {
+//     result += characters.charAt(Math.floor(Math.random() * 
+// charactersLength));
+//  }
+//  return result;
+// }
+
+// class User {
+//   constructor(id,name, studentID) {
+//     this.id = id;
+//     this.name = name;
+//     this.studentId = studentID;
+//     this.time = 0;
+//     this.status = 0;
+//     this.isHost = false;
+//     this.isAdmin = false;
+//     this.online = true;
+//   }
+// }
+// class Message {
+//   constructor(name,content,isSelf) {
+//     this.name = name;
+//     this.content = content;
+//     this.isSelf = isSelf;
+//   } 
+// }
+exports.registerHandler = (io)=>{
+  io.on("connection", socket => {
+    console.log("Socket connected! | UUID:",socket.id)
+  });
+}
